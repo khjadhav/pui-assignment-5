7f0e9bd484b39d72af65020e6d357249b621f751
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-10-22 17:00:34 -0400

    comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// create a Bun constructor
 function Bun(name, cost, quant, amount, flava1, flava2, img){
     this.name=name;
     this.cost=cost;
@@ -11,23 +11,32 @@ function Bun(name, cost, quant, amount, flava1, flava2, img){
 
 $(document).ready(function() {
 // -----------------------------------------Cart Array------------------------------------------
-var cartArray = JSON.parse(localStorage.getItem("cartArray")) || [];
-    $("#numForCart").text(cartArray.length);
 
+// Create an array from the data stored in local storage
+var cartArray = JSON.parse(localStorage.getItem("cartArray")) || [];
+// Change the number of items in the shopping cart
+$("#numForCart").text(cartArray.length);
+
+// loop through the carArray
 for (var i=0; i<cartArray.length; i++){
     var desiredBun = cartArray[i];
-    var container = $("<div></div>",{class:'checkout-product-div'});
+    //Dynamically create a div (checkout-product-div) in which all the new divs will go into
+    var container = $("<div></div>",{id:i, class:'checkout-product-div'});
+    // Add the previously created divs to the div called checkout-cart on the checkout page
     container.appendTo($(".checkout-cart"));
 
-
+    // create divs for all the product values to be displayed and add them to checkout-product-div
     $("<img/>",{class:'checkout-image-super',src:desiredBun.img}).appendTo(container);
     console.log($('.checkout-image-super').length);
+
 
     $("<div id=product-cost>"+"$ "+desiredBun.cost*desiredBun.amount+"</div>").appendTo(container);
     $("<h1 id=product-name>"+desiredBun.name+"</h1>").appendTo(container);
 
     $("<div id=product-quant>"+"Chosen Pack: "+desiredBun.quant+"</div>").appendTo(container);
     $("<div id=product-amount>"+"Quantity: "+desiredBun.amount+"</div>").appendTo(container);
+
+    //Create the flavour divs only when the flavours are selected
     if (desiredBun.flava1!=null){
     $("<div id=additionalFlavours>"+"Additional Flavours"+"</div>").appendTo(container);
     }
@@ -41,30 +50,30 @@ for (var i=0; i<cartArray.length; i++){
     $("<div id=product-flava2>"+"  2. "+desiredBun.flava2+"</div>").appendTo(container);
     }
 
-
-    var deleteButton = $("<button id=" +i+ ">Remove Item</button>");
+    // dynamically create a delete button and append it to the container
+    var deleteButton = $("<button id=" +i+ " class='del-button'>Remove Item</button>");
     deleteButton.appendTo(container);
 
-    deleteButton.click(function(){
+    // Create a function to delete items from the cart
+    $(".del-button").click(function(){
         console.log("clicked!");
         $(this).parent().remove();
-        var cartArray = JSON.parse(localStorage.getItem("cartArray")) || [];
-
-        // cartArray.splice(cartArray.indexOf($(this).parent(),1);
-        // cartArray.splice( $.inArray(i, cartArray), 1 );
-        // cartArray.splice(i,1)
+        var position=$(this).attr('id');
+        cartArray.splice(position,1);
+        console.log(cartArray);
+        localStorage.setItem("cartArray", JSON.stringify(cartArray));
         $("#numForCart").text(cartArray.length);
         location.reload();
     });
-
     $(".checkout-cart").append(container);
 }
 
+//hide the flavour dropdowns when the page loads first
 $("#select-text").hide();
 $("#flavour1").hide();
 $("#flavour2").hide();
 
-
+// Assign variables for all the product values
 var price=1.00,location="assets/original(gluten-free).png", number=1, amountOfProduct=1;
 $("#packs").change(function(){
         if (this.value == "Pack of 1"){
@@ -98,39 +107,29 @@ $("#packs").change(function(){
             }
         })
 
+//Create function to add items to the cart
 $(".add-to-cart-button").click(function(){
+    //get values of the input items and store them as variables
     var packs= $("#packs").val();
     var flav1= $("#flavour1").val();
     var flav2= $("#flavour2").val();
     var amountOfProduct=$("#number-input").val();
+
+    // Create a new BunBun using the newly extracted information from the page
     var BunBun=new Bun("Original (Gluten-Free)", price, packs, amountOfProduct, flav1, flav2, location);
     console.log(location);
 
-    // || means put the things in an Array
+    // Add items to the local storage
     var existingCartItems=JSON.parse(localStorage.getItem("cartArray")) || [];
     existingCartItems.push(BunBun);
     localStorage.setItem("cartArray", JSON.stringify(existingCartItems));
     console.log(JSON.parse(localStorage.getItem("cartArray")));
 
+    //Display the number of items in the cart near the cart icon
     var numForCart = parseInt($("#numForCart").text());
     console.log(numForCart);
     numForCart+= parseInt($("#number-input").val());
     $("#numForCart").text(numForCart);
-})
-
-$(".add-to-wishlist-button").click(function(){
-    var packs= $("#packs").val();
-    var flav1= $("#flavour1").val();
-    var flav2= $("#flavour2").val();
-    var amountOfProduct=$("#number-input").val();
-    var BunBun=new Bun("Original (Gluten-Free)", price, packs, amountOfProduct, flav1, flav2, location);
-    console.log(location);
-
-    // || means put the things in an Array
-    var existingCartItems=JSON.parse(localStorage.getItem("wishArray")) || [];
-    existingCartItems.push(BunBun);
-    localStorage.setItem("wishArray", JSON.stringify(existingCartItems));
-    console.log(JSON.parse(localStorage.getItem("wishArray")));
 })
 
 })
